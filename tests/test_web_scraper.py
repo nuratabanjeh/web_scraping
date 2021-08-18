62f3ad7d97958e6330f54d5d31269fa864b8996e
nuratabanjeh <nuratabanjeh96@gmail.com>
2021-08-19 02:19:47 +0300

    finishLab17

--- tests/test_web_scraper.py
+++ tests/test_web_scraper.py
@@ -3,3 +3,18 @@ from web_scraper import __version__
 
 def test_version():
     assert __version__ == '0.1.0'
+
+
+from web_scraper.scraper import *
+
+def test_get_citations_needed_number():
+    url="https://en.wikipedia.org/wiki/History_of_Mexico"
+    actual = get_citations_needed_number(url)
+    expected = 5
+    assert actual == expected
+
+def test_get_citations_needed_report():
+    url="https://en.wikipedia.org/wiki/History_of_Mexico"
+    actual = get_citations_needed_report(url)
+    expected = "During the three centuries of colonial rule, fewer than 700,000 Spaniards, most of them men, settled in Mexico.[citation needed] Europeans, Africans, and indigenous intermixed, creating a mixed-race casta population in a process known as mestizaje. Mestizos, people of mixed European-indigenous ancestry, constitute the majority of Mexico's population."
+    assert actual == expected
--- web_scraper/scraper.py
+++ web_scraper/scraper.py
@@ -1,6 +1,28 @@
 import requests
-from bs4 import BeautifulSoup
- 
-URL =""
-page = requests.get(URL)
-print(page)
+from bs4 import BeautifulSoup 
+
+URL="https://en.wikipedia.org/wiki/History_of_Mexico"
+page=requests.get(URL)
+
+soup=BeautifulSoup(page.content,'html.parser')
+
+def get_citations_needed_number(url):
+    count=[]
+    for wrapper in soup.find("div",class_="mw-parser-output").find_all("p"):
+        for wrapperr in wrapper.find_all('a', title="Wikipedia:Citation needed"):
+            a=wrapperr
+            count.append(a)
+
+    print("How many citations? the Citations =",len(count))
+    return len(count)    
+
+def  get_citations_needed_report(url):
+    for wrapper in soup.find("div",class_="mw-parser-output").find_all("p"):
+        for wrapperr in wrapper.find_all('a', title="Wikipedia:Citation needed"):
+            a=wrapperr.parent.parent.parent.text.strip()
+            print("\n",a,"\n")
+    return a
+
+if __name__ == "__main__":
+    get_citations_needed_number(URL)
+    get_citations_needed_report(URL)
